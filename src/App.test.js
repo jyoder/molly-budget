fb79494c9fc43b420201f75f10ed7456d811a49c
John Yoder <john.yoder@appfolio.com>
2018-03-12 22:25:21 -0700

    Remove default boilerplate and setup Enzyme testing

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import App from 'App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+import React from 'react';
+import { shallow } from 'enzyme';
+
+describe('App', () => {
+    it('renders hello world', () => {
+        const app = shallow(<App />);
+        expect(app.text()).toBe('Hello world!');
+    });
 });
