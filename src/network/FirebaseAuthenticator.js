4bde5f9cbf0bc6a05d1a95ed2c1f4739a3ce9fa4
John Yoder <john.yoder@appfolio.com>
2018-03-14 01:23:34 -0700

    Add the FirebaseAuthenticator to authenticate a user

---
+++ src/network/FirebaseAuthenticator.js
@@ -0,0 +1,30 @@
+import * as firebase from 'firebase/app';
+import 'firebase/auth';
+
+export default class FirebaseAuthenticator {
+    static create(firebase) {
+        return new FirebaseAuthenticator(firebase.auth());
+    }
+
+    constructor(firebaseAuth) {
+        this._firebaseAuth = firebaseAuth;
+    }
+
+    authenticate(onAuthenticated) {
+        this._firebaseAuth.onAuthStateChanged((user) => {
+            if(user) {
+                onAuthenticated(user);
+            } else {
+                this._signIn();
+            }
+        });
+    }
+
+    _signIn() {
+        this._firebaseAuth.signInWithRedirect(
+            new firebase.auth.GoogleAuthProvider()
+        ).catch((error) => {
+            console.log(error);
+        });
+    }
+}
---
+++ src/network/FirebaseAuthenticator.test.js
@@ -0,0 +1,46 @@
+import FirebaseAuthenticator from 'network/FirebaseAuthenticator';
+
+import * as firebase from 'firebase/app';
+import 'firebase/auth';
+
+describe('authenticate', () => {
+    it('awaits an authentication state change from firebase', () => {
+        const firebaseAuth = { onAuthStateChanged: jest.fn() };
+        const googleAuthProvider = {};
+        const authenticator = new FirebaseAuthenticator(firebaseAuth, googleAuthProvider);
+        
+        authenticator.authenticate(() => {});
+        expect(firebaseAuth.onAuthStateChanged).toHaveBeenCalledTimes(1);
+    });
+
+    it('invokes the given callback when the user is authenticated', () => {
+        const firebaseAuth = { onAuthStateChanged: jest.fn() };
+        const googleAuthProvider = {};
+        const authenticator = new FirebaseAuthenticator(firebaseAuth, googleAuthProvider);
+        const onAuthenticated = jest.fn();
+
+        authenticator.authenticate(onAuthenticated);
+        firebaseAuth.onAuthStateChanged.mock.calls[0][0]('John');
+        expect(onAuthenticated).toHaveBeenCalledWith('John');
+    });
+
+    it('asks the user to sign in if they have not yet been authenticated', () => {
+        const catchClause = {};
+        catchClause['catch'] = jest.fn();
+
+        const firebaseAuth = {
+            onAuthStateChanged: jest.fn(),
+            signInWithRedirect: jest.fn(() => catchClause)
+        };
+        const googleAuthProvider = {};
+        const authenticator = new FirebaseAuthenticator(firebaseAuth, googleAuthProvider);
+        const onAuthenticated = jest.fn();
+
+        authenticator.authenticate(onAuthenticated);
+        firebaseAuth.onAuthStateChanged.mock.calls[0][0](null);
+        expect(onAuthenticated).not.toHaveBeenCalled();
+        expect(firebaseAuth.signInWithRedirect.mock.calls[0][0])
+            .toBeInstanceOf(firebase.auth.GoogleAuthProvider);
+        expect(catchClause.catch).toHaveBeenCalledTimes(1);
+    });
+});
