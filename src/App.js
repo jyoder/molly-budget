c02179850f370324e9d088924c294450ef00855a
John Yoder <john.yoder@appfolio.com>
2018-03-14 01:23:53 -0700

    Update the top-level App to perform authentication

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
 import React from 'react';
+import FirebaseProvider from 'network/FirebaseProvider';
+import FirebaseAuthenticator from 'network/FirebaseAuthenticator';
 
 export default class App extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = { user: null };
+    }
+
+    componentWillMount() {
+        const firebase = document.firebaseProvider || FirebaseProvider.create().getFirebase();
+        FirebaseAuthenticator.create(firebase).authenticate((user) => {
+            this.setState({ user: user });
+        });
+    }
+
     render() {
-        return(<h1>Hello world!</h1>);
+        if(this.state.user) {
+            return(<h1>Hello {this.state.user.displayName}!</h1>);
+        } else {
+            return(<h1>Authenticating...</h1>);
+        }
     }
 }
--- src/App.test.js
+++ src/App.test.js
@@ -4,8 +4,27 @@ import React from 'react';
 import { shallow } from 'enzyme';
 
 describe('App', () => {
-    it('renders hello world', () => {
+    it('renders authenticating if the user has not yet authenticated', () => {
+        const auth = jest.fn(() => ({
+            onAuthStateChanged: jest.fn()
+        }));
+        document.firebaseProvider = { auth: auth };
+
         const app = shallow(<App />);
-        expect(app.text()).toBe('Hello world!');
+        expect(app.text()).toBe('Authenticating...');
+    });
+
+    it('renders a greeting to the user if he or she has authenticated', () => {
+        const onAuthStateChanged = jest.fn();
+        const auth = jest.fn(() => ({
+            onAuthStateChanged: onAuthStateChanged
+        }));
+        document.firebaseProvider = { auth: auth };
+
+        const app = shallow(<App />);
+        auth().onAuthStateChanged.mock.calls[0][0]({displayName: 'Larry Bird'});
+        
+        app.update();
+        expect(app.text()).toBe('Hello Larry Bird!');
     });
 });
