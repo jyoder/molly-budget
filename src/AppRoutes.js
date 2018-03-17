0a6f0d2032b8d0e7305b176404b265720e0ca574
John Yoder <john.yoder@appfolio.com>
2018-03-17 12:51:08 -0700

    Add AppRoutes for navigating between pages

---
+++ src/AppRoutes.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
+import BudgetSummary from 'BudgetSummary';
+import Transactions from 'Transactions';
+
+export default class AppRoutes extends React.Component {
+    render() {
+        return(
+            <Router>
+                <div>
+                    <Switch>
+                        <Route exact path="/" render={({ history }) => (
+                            <BudgetSummary
+                                user={this.props.user}
+                                budget={this.props.budget}
+                                history={history}
+                            />)}
+                        />
+                    
+                        <Route path="/transactions" render={({ history }) => (
+                            <Transactions
+                                history={history}
+                            />)}
+                        />
+                    </Switch>
+                </div>
+            </Router>
+        );
+    }
+}
+
+AppRoutes.propTypes = {
+    user: PropTypes.object.isRequired,
+    budget: PropTypes.object.isRequired
+};
---
+++ src/AppRoutes.test.js
@@ -0,0 +1,35 @@
+import AppRoutes from 'AppRoutes';
+import BudgetSummary from 'BudgetSummary';
+import Transactions from 'Transactions';
+
+import Budget from 'Budget';
+
+import React from 'react';
+import { MemoryRouter } from 'react-router-dom';
+import { shallow, mount } from 'enzyme';
+
+describe('AppRoutes', () => {
+    it('renders BudgetSummary when the user navigates to /', () => {
+        const user = {};
+        const budget = Budget.create(10.00);
+        const wrapper = mount(
+            <MemoryRouter initialEntries={['/']}>
+                <AppRoutes user={user} budget={budget} />
+            </MemoryRouter>
+        );
+        expect(wrapper.find(BudgetSummary)).toHaveLength(1);
+        expect(wrapper.find(Transactions)).toHaveLength(0);
+    });
+
+    it('renders Transactions when the user navigates to /transactions', () => {
+        const user = {};
+        const budget = Budget.create(10.00);
+        const wrapper = mount(
+            <MemoryRouter initialEntries={['/transactions']}>
+                <AppRoutes user={user} budget={budget}/>
+            </MemoryRouter>
+        );
+        expect(wrapper.find(BudgetSummary)).toHaveLength(0);
+        expect(wrapper.find(Transactions)).toHaveLength(1);
+    });
+});
