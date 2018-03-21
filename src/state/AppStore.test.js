de8fa87cf85ff87d4792d9081d93ab136c684d52
John Yoder <john.yoder@appfolio.com>
2018-03-21 00:49:27 -0700

    Add some missing tests for the AppStore

--- src/state/AppStore.test.js
+++ src/state/AppStore.test.js
@@ -1,6 +1,27 @@
 import AppStore from 'state/AppStore';
 
 
+describe('initialized', () => {
+    it('returns false if the user is missing', () => {
+        const appStore = new AppStore();
+        appStore.setTransactionStore({});
+        expect(appStore.initialized).toBeFalsy();
+    });
+
+    it('returns false if the transaction store is missing', () => {
+        const appStore = new AppStore();
+        appStore.setUser({});
+        expect(appStore.initialized).toBeFalsy();
+    });
+
+    it('returns true if the user and transaction store are present', () => {
+        const appStore = new AppStore();
+        appStore.setUser({});
+        appStore.setTransactionStore({});
+        expect(appStore.initialized).toBeTruthy();
+    });
+});
+
 describe('getUser', () => {
     it('returns the user', () => {
         const appStore = new AppStore();
