037f0be545df28909e750ff5cec1d05b5752db4c
John Yoder <john.yoder@appfolio.com>
2018-03-21 00:34:14 -0700

    Add a dollar sign in front of the CurrencyInput

--- src/ui/transaction/CurrencyInput.js
+++ src/ui/transaction/CurrencyInput.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { observer } from 'mobx-react';
-import { Input } from 'reactstrap';
+import { Input, InputGroup, InputGroupAddon, InputGroupText } from 'reactstrap';
 import PropTypes from 'prop-types';
 
 import 'ui/transaction/CurrencyInput.css';
@@ -9,14 +9,20 @@ import 'ui/transaction/CurrencyInput.css';
 class CurrencyInput extends React.Component {
     render() {
         return(
-            <div className="CurrencyInput">                    
-                <Input
-                    type="number"
-                    step="0.1"
-                    onChange={(event) => this._onChange(event)}
-                    placeholder="Enter transaction amount"
-                    bsSize="lg"
-                />             
+            <div className="CurrencyInput">
+                <InputGroup className="PurchasePage-Amount">
+                    <InputGroupAddon addonType="prepend">
+                        <InputGroupText>$</InputGroupText>
+                    </InputGroupAddon>
+
+                    <Input
+                        type="number"
+                        step="0.1"
+                        onChange={(event) => this._onChange(event)}
+                        placeholder="Enter transaction amount"
+                        bsSize="lg"
+                    />
+                </InputGroup>             
             </div>
         );
     }
