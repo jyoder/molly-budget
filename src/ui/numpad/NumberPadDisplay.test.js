d29dde2321d5c0310edc638a8ea07cdc22b46362
John Yoder <john.yoder@appfolio.com>
2018-03-30 14:55:29 -0700

    Add the NumberPadDisplay

---
+++ src/ui/numpad/NumberPadDisplay.test.js
@@ -0,0 +1,30 @@
+import NumberPadDisplay from 'ui/numpad/NumberPadDisplay';
+import ValueStore from 'state/ValueStore';
+
+import React from 'react';
+import { shallow } from 'enzyme';
+
+
+describe('NumberPadDisplay', () => {
+    it('renders "$0.00" when the store is empty', () => {
+        const valueStore = new ValueStore();
+        const numberPadDisplay = shallow(<NumberPadDisplay valueStore={valueStore} />);
+        expect(numberPadDisplay.text()).toBe('$0.00');
+    });
+
+    it('renders the value in the store in currency format', () => {
+        const valueStore = new ValueStore();
+        valueStore.setValue(123);
+
+        const numberPadDisplay = shallow(<NumberPadDisplay valueStore={valueStore} />);
+        expect(numberPadDisplay.text()).toBe('$123.00');
+    });
+
+    it('renders a value of zero in currency format', () => {
+        const valueStore = new ValueStore();
+        valueStore.setValue(0);
+
+        const numberPadDisplay = shallow(<NumberPadDisplay valueStore={valueStore} />);
+        expect(numberPadDisplay.text()).toBe('$0.00');
+    });
+});
