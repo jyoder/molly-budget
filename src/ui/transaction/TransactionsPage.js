dafe5eb5bd84d76c047274bd2026a6addb22993a
John Yoder <john.yoder@appfolio.com>
2018-03-30 15:01:16 -0700

    Update the TransactionsPage to include a NumberPad and navigation buttons

--- src/ui/transaction/TransactionsPage.js
+++ src/ui/transaction/TransactionsPage.js
@@ -2,37 +2,54 @@ import React from 'react';
 import { observer } from 'mobx-react';
 import PropTypes from 'prop-types';
 
-import CurrencyInput from 'ui/transaction/CurrencyInput';
-import CurrencyAmountStore from 'ui/transaction/CurrencyAmountStore';
-import SubmitTransactionMenu from 'ui/transaction/SubmitTransactionMenu';
+import NumberPadDisplay from 'ui/numpad/NumberPadDisplay';
+import NumberPad from 'ui/numpad/NumberPad';
+import { Button } from 'reactstrap';
+
+import 'ui/transaction/TransactionsPage.css';
 
 
 class TransactionsPage extends React.Component {
-    constructor(props) {
-        super(props);
-        this._currencyAmountStore = new CurrencyAmountStore();
-    }
-
     render() {
         return(
-            <section>
-                <CurrencyInput
-                    currencyAmountStore={this._currencyAmountStore}
-                />
-                
-                <br />
-                
-                <SubmitTransactionMenu
-                    currencyAmountStore={this._currencyAmountStore}
-                    transactionStore={this.props.transactionStore}
-                    history={this.props.history}
-                />
+            <section className="TransactionsPage">
+                <div className="TransactionsPage-numberInput">
+                    <NumberPadDisplay valueStore={this.props.amountStore} />
+                    <NumberPad valueStore={this.props.amountStore} />
+                </div>
+
+                <Button
+                    size='lg'
+                    outline
+                    block
+                    className="TransactionsPage-button TransactionsPage-chooseCategoryButton"
+                    onClick={() => this._onClickChooseCategory()}>
+                    Choose Category
+                </Button>
+
+                <Button
+                    size='lg'
+                    outline
+                    block
+                    className="TransactionsPage-backButton"
+                    onClick={() => this._onClickGoBack()}>
+                    Go Back
+                </Button>
             </section>
         );
+    }
+
+    _onClickChooseCategory() {
+        this.props.history.push('/categories');
+    }
+
+    _onClickGoBack() {
+        this.props.history.push('/');
     }
 }
 
 TransactionsPage.propTypes = {
+    amountStore: PropTypes.object.isRequired,
     transactionStore: PropTypes.object.isRequired,
     history: PropTypes.object.isRequired
 };
--- src/ui/transaction/TransactionsPage.test.js
+++ src/ui/transaction/TransactionsPage.test.js
@@ -2,32 +2,68 @@ import React from 'react';
 import { shallow } from 'enzyme';
 
 import TransactionsPage from 'ui/transaction/TransactionsPage';
-import CurrencyInput from 'ui/transaction/CurrencyInput';
+import NumberPadDisplay from 'ui/numpad/NumberPadDisplay';
+import NumberPad from 'ui/numpad/NumberPad';
 import SubmitTransactionMenu from 'ui/transaction/SubmitTransactionMenu';
+import { Button } from 'reactstrap';
 
 
 describe('TransactionsPage', () => {
-    it('renders a CurrencyInput', () => {
+    it('renders a NumberPadDisplay', () => {
+        const amountStore = {};
         const transactionStore = {};
         const history = {};
         
         const transactionsPage = shallow(<TransactionsPage
+            amountStore={amountStore}
             transactionStore={transactionStore}
             history={history}
         />);
 
-        expect(transactionsPage.find(CurrencyInput)).toHaveLength(1);
+        expect(transactionsPage.find(NumberPadDisplay)).toHaveLength(1);
     });
 
-    it('renders a SubmitTransactionMenu', () => {
+    it('renders a NumberPad', () => {
+        const amountStore = {};
         const transactionStore = {};
         const history = {};
         
         const transactionsPage = shallow(<TransactionsPage
+            amountStore={amountStore}
             transactionStore={transactionStore}
             history={history}
         />);
 
-        expect(transactionsPage.find(SubmitTransactionMenu)).toHaveLength(1);
+        expect(transactionsPage.find(NumberPad)).toHaveLength(1);
+    });
+
+    it('navigates to /categories when the choose category button is clicked', () => {
+        const amountStore = {};
+        const transactionStore = {};
+        const history = { push: jest.fn() };
+        
+        const transactionsPage = shallow(<TransactionsPage
+            amountStore={amountStore}
+            transactionStore={transactionStore}
+            history={history}
+        />);
+
+        transactionsPage.find('.TransactionsPage-chooseCategoryButton').simulate('click');
+        expect(history.push).toHaveBeenCalledWith('/categories');
+    });
+
+    it('navigates to / when the go back button is clicked', () => {
+        const amountStore = {};
+        const transactionStore = {};
+        const history = { push: jest.fn() };
+        
+        const transactionsPage = shallow(<TransactionsPage
+            amountStore={amountStore}
+            transactionStore={transactionStore}
+            history={history}
+        />);
+
+        transactionsPage.find('.TransactionsPage-backButton').simulate('click');
+        expect(history.push).toHaveBeenCalledWith('/');
     });
 });
