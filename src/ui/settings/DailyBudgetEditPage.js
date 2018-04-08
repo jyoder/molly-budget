ebeb178c74bb0bd4f00c4e67834663a42358688d
John Yoder <john.yoder@appfolio.com>
2018-04-07 22:53:17 -0700

    Add a default daily budget

--- src/ui/settings/DailyBudgetEditPage.js
+++ src/ui/settings/DailyBudgetEditPage.js
@@ -2,16 +2,32 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { observer } from 'mobx-react';
 
+import NumberPad from 'ui/numpad/NumberPad';
+
+import 'ui/app/AppPage.css';
+
+
+const DEFAULT_DAILY_BUDGET = 40;
 
 class DailyBudgetEditPage extends React.Component {
     render() {
         return(
             <div className="AppPage">
                 <p className="DailyBudgetEditPage-dailyBudget lead">
-                    Your daily budget is <strong>${this.props.dailyBudgetStore.value()}</strong>
+                    Your daily budget is <strong>${this._dailyBudget()}</strong>
                 </p>
+
+                <NumberPad valueStore={this.props.dailyBudgetStore} />
             </div>
         );
+    }
+
+    _dailyBudget() {
+        if(this.props.dailyBudgetStore.value() !== null) {
+            return Math.trunc(this.props.dailyBudgetStore.value());
+        } else {
+            return DEFAULT_DAILY_BUDGET;
+        }
     }
 }
 
--- src/ui/settings/DailyBudgetEditPage.test.js
+++ src/ui/settings/DailyBudgetEditPage.test.js
@@ -2,17 +2,51 @@ import React from 'react';
 import { shallow } from 'enzyme';
 
 import DailyBudgetEditPage from 'ui/settings/DailyBudgetEditPage';
+import NumberPad from 'ui/numpad/NumberPad';
 import ValueStore from 'state/ValueStore';
 
 
 describe('DailyBudgetEditPage', () => {
+    it('renders a default value of $40 if no value is supplied', () => {
+        const dailyBudgetStore = new ValueStore();
+        const dailyBudgetEditPage = shallow(
+            <DailyBudgetEditPage
+                dailyBudgetStore={dailyBudgetStore}
+        />);
+
+        expect(dailyBudgetEditPage.find('.DailyBudgetEditPage-dailyBudget').text())
+            .toBe('Your daily budget is $40');
+    });
+
     it('renders the current daily budget', () => {
         const dailyBudgetStore = new ValueStore(20);
-        const dailyBudgetEditPage = shallow(<DailyBudgetEditPage
-            dailyBudgetStore={dailyBudgetStore}
+        const dailyBudgetEditPage = shallow(
+            <DailyBudgetEditPage
+                dailyBudgetStore={dailyBudgetStore}
         />);
 
         expect(dailyBudgetEditPage.find('.DailyBudgetEditPage-dailyBudget').text())
             .toBe('Your daily budget is $20');
+    });
+
+    it('renders the current daily budget, truncated to a whole number', () => {
+        const dailyBudgetStore = new ValueStore(20.1234);
+        const dailyBudgetEditPage = shallow(
+            <DailyBudgetEditPage
+                dailyBudgetStore={dailyBudgetStore}
+        />);
+
+        expect(dailyBudgetEditPage.find('.DailyBudgetEditPage-dailyBudget').text())
+            .toBe('Your daily budget is $20');
+    });
+
+    it('renders a NumberPad', () => {
+        const dailyBudgetStore = new ValueStore(20);
+        const dailyBudgetEditPage = shallow(
+            <DailyBudgetEditPage
+                dailyBudgetStore={dailyBudgetStore}
+        />);
+
+        expect(dailyBudgetEditPage.find(NumberPad)).toHaveLength(1);
     });
 });
