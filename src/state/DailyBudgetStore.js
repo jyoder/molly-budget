86a0f1567e356fc8fb468472ffc006869a62f527
John Yoder <john.yoder@appfolio.com>
2018-04-09 22:40:04 -0700

    Add the DailyBudgetStore

---
+++ src/state/DailyBudgetStore.js
@@ -0,0 +1,61 @@
+import { decorate, observable } from 'mobx';
+import DailyBudget from 'state/DailyBudget';
+import DailyBudgetSerializer from 'state/DailyBudgetSerializer';
+
+
+const DEFAULT_DAILY_BUDGET = new DailyBudget(40.00, new Date('0000-01-01T00:00:00.000Z'));
+
+class DailyBudgetStore {
+    static create(firebaseDatabase, userId, onInitialized) {
+        const dailyBudgetsRef = firebaseDatabase.ref(`/accounts/${userId}/dailyBudgets`);
+        const dailyBudgetStore = new DailyBudgetStore(dailyBudgetsRef, []);
+        this._initialize(dailyBudgetsRef, dailyBudgetStore, onInitialized);
+
+        return dailyBudgetStore;
+    }
+
+    static _initialize(dailyBudgetRef, dailyBudgetStore, onInitialized) {
+        dailyBudgetRef.on('value', (snapshot) => {
+            if(snapshot.val()) {
+                dailyBudgetStore.receiveDailyBudgets(
+                    DailyBudgetSerializer.fromJsonList(Object.values(snapshot.val()))
+                );
+            }
+            if(onInitialized) {
+                onInitialized(dailyBudgetStore);
+                onInitialized = null;
+            }
+        });
+    }
+
+    constructor(dailyBudgetsRef, dailyBudgets) {
+        this._dailyBudgetsRef = dailyBudgetsRef;
+        this._dailyBudgets = dailyBudgets;
+    }
+
+    dailyBudgets() {
+        return this._dailyBudgets.slice();
+    }
+
+    currentDailyBudget() {
+        if(this._dailyBudgets.length > 0) {
+            return this._dailyBudgets[this._dailyBudgets.length - 1];
+        } else {
+            return DEFAULT_DAILY_BUDGET;
+        }
+    }
+
+    addDailyBudget(amount, createdAt, category) {
+        const ref = this._dailyBudgetsRef.push();
+        const dailyBudget = new DailyBudget(ref.key, amount, createdAt, category);
+        ref.set(DailyBudgetSerializer.toJson(dailyBudget));
+    }
+
+    receiveDailyBudgets(dailyBudgets) {
+        this._dailyBudgets = dailyBudgets;
+    }
+}
+
+export default decorate(DailyBudgetStore, {
+    _dailyBudgets: observable
+});
