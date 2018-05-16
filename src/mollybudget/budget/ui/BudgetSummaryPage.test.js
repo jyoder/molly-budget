37c4826d705003662d083c8fea89757f1a62afeb
John Yoder <john.yoder@appfolio.com>
2018-05-15 20:28:00 -0700

    Correct a typo in the BudgetSummaryPage tests

--- src/mollybudget/budget/ui/BudgetSummaryPage.test.js
+++ src/mollybudget/budget/ui/BudgetSummaryPage.test.js
@@ -1,19 +1,19 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 
-import BudgetSummary from 'mollybudget/budget/ui/BudgetSummaryPage';
+import BudgetSummaryPage from 'mollybudget/budget/ui/BudgetSummaryPage';
 import Budget from 'mollybudget/budget/model/Budget';
 import DailyBudget from 'mollybudget/settings/model/DailyBudget';
 import { Button } from 'reactstrap';
 
 
-describe('BudgetSummary', () => {
+describe('BudgetSummaryPage', () => {
     it('renders the current user\'s display name and current budget', () => {
         const user = { displayName: 'Fred Rogers' };
         const dailyBudget = new DailyBudget('id1', 50.00, new Date('2018-02-12T11:00:00.000Z'));
         const budget = new Budget(new Date('2018-02-15T11:00:00.000Z'), [dailyBudget], []);
         const history = { push: jest.fn() };
-        const budgetSummary = shallow(<BudgetSummary user={user} budget={budget} history={history}/>);
+        const budgetSummary = shallow(<BudgetSummaryPage user={user} budget={budget} history={history}/>);
 
         expect(budgetSummary.find('.BudgetSummaryPage-summary').text())
             .toBe('Hello Fred Rogers,you have $150.00 to spend today.');
@@ -23,7 +23,7 @@ describe('BudgetSummary', () => {
         const user = { displayName: 'Fred Rogers' };
         const budget = new Budget(new Date('2018-02-15T11:00:00.000Z'), [], []);
         const history = { push: jest.fn() };
-        const budgetSummary = shallow(<BudgetSummary user={user} budget={budget} history={history}/>);
+        const budgetSummary = shallow(<BudgetSummaryPage user={user} budget={budget} history={history}/>);
 
         budgetSummary.find('.BudgetSummaryPage-addTransaction').simulate('click');
         expect(history.push).toHaveBeenCalledWith('/transactions/new');
@@ -33,7 +33,7 @@ describe('BudgetSummary', () => {
         const user = { displayName: 'Fred Rogers' };
         const budget = new Budget(new Date('2018-02-15T11:00:00.000Z'), [], []);
         const history = { push: jest.fn() };
-        const budgetSummary = shallow(<BudgetSummary user={user} budget={budget} history={history}/>);
+        const budgetSummary = shallow(<BudgetSummaryPage user={user} budget={budget} history={history}/>);
 
         budgetSummary.find('.BudgetSummaryPage-viewPastTransactions').simulate('click');
         expect(history.push).toHaveBeenCalledWith('/transactions');
